875ada9d8e2128e3902d8b9aaf7f7254a11cfd38
mdsalahuddin2001 <mdsalahuddin46464@gmail.com>
2025-09-19 18:59:34 +0600

    feat: tooling added

--- test/globalTeardown.ts
+++
@@ -1,4 +0,0 @@
-export default async function globalTeardown(): Promise<void> {
-  const instance = global.__MONGOINSTANCE as any;
-  await instance.stop();
-}
--- test/ignore-test.ts
+++
@@ -1,7 +0,0 @@
-import * as request from 'supertest'
-describe('Ignore Test', () => {
-  it('should return 200 OK', async () => {
-    const response = await request('http://localhost:4000').get('/health')
-    expect(response.status).toBe(200)
-  })
-})
